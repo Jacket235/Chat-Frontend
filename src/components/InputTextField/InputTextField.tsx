644babdb8813527c9a3fd3f4ec4de10fcd3b78ba
Sebastian Mastalerz <sebastian.masti235@gmail.com>
2026-02-27 12:00:18 +0100

    InputTextField Component

---
+++ src/components/InputTextField/InputTextField.tsx
@@ -0,0 +1,9 @@
+import './input-text-field.scss'
+
+export default function InputTextField() {
+    return(
+        <label className='input-text-field'>
+            <input />
+        </label>
+    )
+}
