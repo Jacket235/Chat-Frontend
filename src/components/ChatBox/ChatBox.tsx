a27f5d6729cc716bc2c879dbf7409c9e28a7190a
Sebastian Mastalerz <sebastian.masti235@gmail.com>
2026-02-27 10:50:18 +0100

    ChatBox fake connection for now

--- src/components/ChatBox/ChatBox.tsx
+++ src/components/ChatBox/ChatBox.tsx
@@ -1,12 +1,18 @@
-import Message from '../Message/Message'
+import { useState } from 'react'
+import Button from '../Button/Button'
 import './chat-box.scss'
 
 export default function ChatBox() {
+    const [connected, setConnected] = useState<boolean>(false);
+
+    const handleConnect = () => {
+        setConnected(true)
+    }
+
     return(
         <div className="chat-box">
             <div className='chat-box-container'>
-                <Message text='Hej'/>
-                <Message text='This is a message bigger than the average one' reply/>
+                <Button label='Dołącz' onClick={handleConnect} disabled={connected}/>
             </div>
         </div>
     )
